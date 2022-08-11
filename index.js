c66eee289ed9e391b15cc31198969d27255e10d5
Muhammed <faheem@metrictreelabs.com>
2022-08-11 09:34:10 +0530

    user api integrated

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 var app = express();
 var cors = require("cors");
+var mongoose = require("./services/mongoose");
 app.use(cors());
 require("dotenv").config();
 const path = require("path");
+
+const router = require("./routes/user.router");
+
+const User = require("./models/user.models");
 
 const http = require("http");
 const { Server } = require("socket.io");
@@ -11,35 +16,51 @@ const { success, error } = require("consola");
 const socketio = require("socket.io");
 
 const server = http.createServer(app);
-
 const io = new Server(server, {
   cors: {
-    origin: "http://192.168.10.79:3000",
+    // origin: "http://192.168.10.79:3000",
+    origin: "http://localhost:3000",
+
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
     credentials: true,
   },
 });
 
+const temp = [{}];
+
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
 
   socket.on("join_room", (data) => {
+    // console.log(data);
     socket.join(data);
   });
 
   socket.on("name", (data) => {
+    console.log(data, "hh");
     socket.join(data);
   });
+
+  socket.on("ferret", (name, fn) => {
+    User.find({}).then((data) => {
+      fn(data);
+    });
+  });
+
   socket.on("send_message", (data) => {
+    console.log(data);
     socket.to(data.room).emit("receive_message", data);
     socket.to(data.name).emit("receive_message", data);
   });
 });
 
-// server.listen(3001, () => {
-//   console.log("SERVER IS RUNNING");
-// });
+const dbName = "myChat";
+mongoose(`mongodb://0.0.0.0:27017/${dbName}`);
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+app.use("/", router);
 server.listen(process.env.PORT, () => {
   success({
     message: `Successfully connected to socket `,
